a0c645663d5a94d3a549c431565d1b41365d59e4
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-12-17 18:30:32 -0300

    Carrossel das habilidades e integração com o bootstrap

---
+++ data.js
@@ -0,0 +1,32 @@
+export const MY_SKILLS = [
+  {
+    name: "HTML, CSS & JAVASCRIPT",
+    description: "Os blocos básicos para construção de código na web. Habilidades relacionadas que gostaria de enfatizar são manipulação de arrays com HOFs, promises e Funções assíncronas, manipulação do DOM.",
+    image: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/83/e258e0532611e5a5072321239ff4d4/jhep-coursera-course4.png?auto=format%2Ccompress&dpr=1",
+  },
+  {
+    name: "REACT",
+    description: "O framework mais utilizado para front-end. Além do conhecimento do ciclo de vida do react, também consigo utilizar componentes relacionados como o “React Router”, “React Testing Library” (RTL) e “Context API”.",
+    image: "https://miro.medium.com/max/1400/0*k9CL2yoHU6ELTkmi.png",
+  },
+  {
+    name: "JEST",
+    description: "Jest é um poderoso Framework de Testes em JavaScript com um foco na simplicidade. Habilidades relacionadas que gostaria de enfatizar são a capacidade de fazer testes unitário, testes assíncronos e simulação de funções (mock).",
+    image: "https://cdn.auth0.com/blog/testing-react-with-jest/logo.png",
+  },
+  {
+    name: "REDUX",
+    description: "Redux é uma biblioteca JavaScript para gerenciamento de estado. Além da criação das actions, reducers e stores, também consigo utilizar o redux-thunk, um middleware para redux que permite escrever funções com lógica que podem interagir com os “dispatchs” e os “getStates”.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png",
+  },
+  {
+    name: "Git & Github",
+    description: "Git é a ferramenta de controle de versionamento de código. Github é a plataforma que os meus código estão hospedados.",
+    image: "https://videosdeti.com.br/wp-content/uploads/2018/12/git-githu-cover.png",
+  },
+  {
+    name: "Linux",
+    description: "O kernel mais famoso do mundo. Habilidades que gostaria de enfatizar são o dominio do terminal e criação de scripts em bash.",
+    image: "https://www.oficinadanet.com.br/imagens/post/25027/tux.png",
+  },
+];
